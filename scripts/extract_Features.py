c3d231fb4dbec013b351fa54a0092502a478dd04
rohith <rrohith2001@gmail.com>
2021-05-18 20:12:47 +0530

    Extraction with Timeout

--- scripts/extract_Features.py
+++ scripts/extract_Features.py
@@ -6,12 +6,13 @@ import pandas as pd
 import numpy as np
 import whois
 from tqdm import tqdm
+from interruptingcow import timeout
 import os
 print(os.getcwd())
-os.chdir(os.getcwd()+'/FinalDataset/URL/')
+os.chdir('../FinalDataset/URL')
 
 #l = ['DefacementSitesURLFiltered.csv','phishing_dataset.csv','Malware_dataset.csv','spam_dataset.csv','Benign_list_big_final.csv']
-l = ['Malware_dataset.csv']#,'spam_dataset.csv','Benign_list_big_final.csv']
+l = ['Benign_list_big_final.csv']
 
 
 emp = data_creation_v3.UrlFeaturizer("").run().keys()
@@ -19,12 +20,15 @@ A = pd.DataFrame(columns = emp)
 t=[]
 for j in l:
     print(j)
-    d=pd.read_csv(j,header=None).to_numpy().flatten()
+    d=pd.read_csv(j,header=None).sample(11000).to_numpy().flatten()
     for i in tqdm(d):
-        temp=data_creation_v3.UrlFeaturizer(i).run()
-        temp["File"]=j.split(".")[0]
-        t.append(temp)
-        #print(t)
+        try: 
+            with timeout(100, exception = RuntimeError):  
+                temp=data_creation_v3.UrlFeaturizer(i).run()
+                temp["File"]=j.split(".")[0]
+                t.append(temp)
+        except RuntimeError: 
+            pass 
 A=A.append(t)
 os.chdir('../')
-A.to_csv("malware_features_v3.1.csv")
+A.to_csv("benign_features.csv")
